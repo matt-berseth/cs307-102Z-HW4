9fea6a4947432fb14f5fc1502bb00b061b42600c
matt-berseth <matt.berseth@nlplogix.com>
2024-02-22 04:29:33 -0500

    initial commit

---
+++ banking_system.py
@@ -0,0 +1,20 @@
+def main():
+   # Initialize the account balance and transaction history
+   balance = 0.0
+   transactions = []
+
+   while True:
+      print("\n--- Basic Banking System ---")
+      print("1: Deposit Money")
+      print("2: Withdraw Money")
+      print("3: Check Balance")
+      print("4: View Transaction History")
+      print("5: Exit")
+      
+      choice = input("Enter your choice: ")
+
+      # TODO: fill in the logic that implements the program here!
+
+
+if __name__ == "__main__":
+   main()
